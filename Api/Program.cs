3a0f339b076f9f9679b19b75053cb8ac0f85ae45
Olga Nosova <nosova.olgs@gmail.com>
2022-09-01 18:25:53 +0200

    added logging

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,48 +1,93 @@
 using Core;
 using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Diagnostics;
 using Prometheus;
+using Serilog;
+using System.Text.Json;
+using static System.Net.Mime.MediaTypeNames;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Warning()
+    .WriteTo.Http(
+        Environment.GetEnvironmentVariable("LOGSTASH_URI")
+            ?? throw new ArgumentNullException("Logstash URI is needed"),
+        null)
+    .CreateLogger();
 
-// Add services to the container.
-
-// Add services to the container.
-builder.Services.AddSingleton<IMessageRepository>(sp => new MessageRepository(
-    Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
-        ?? builder.Configuration.GetValue<string>("MongoDbSettings:ConnectionString"),
-    Environment.GetEnvironmentVariable("DB_NAME")
-        ?? builder.Configuration.GetValue<string>("MongoDbSettings:DbName"),
-    Environment.GetEnvironmentVariable("DB_TWEET_COLLECTION")
-        ?? builder.Configuration.GetValue<string>("MongoDbSettings:DbTweetCollectionName"),
-    Environment.GetEnvironmentVariable("DB_REPLY_COLLECTION")
-        ?? builder.Configuration.GetValue<string>("MongoDbSettings:DbReplyCollectionName")
-));
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.AddAutoMapper(typeof(Program));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddSingleton<IMessageRepository>(sp => new MessageRepository(
+        Environment.GetEnvironmentVariable("DB_CONNECTION_STRING")
+            ?? builder.Configuration.GetValue<string>("MongoDbSettings:ConnectionString"),
+        Environment.GetEnvironmentVariable("DB_NAME")
+            ?? builder.Configuration.GetValue<string>("MongoDbSettings:DbName"),
+        Environment.GetEnvironmentVariable("DB_TWEET_COLLECTION")
+            ?? builder.Configuration.GetValue<string>("MongoDbSettings:DbTweetCollectionName"),
+        Environment.GetEnvironmentVariable("DB_REPLY_COLLECTION")
+            ?? builder.Configuration.GetValue<string>("MongoDbSettings:DbReplyCollectionName")
+    ));
+
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    builder.Services.AddAutoMapper(typeof(Program));
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseExceptionHandler(exceptionHandlerApp =>
+        {
+            exceptionHandlerApp.Run(async context =>
+            {
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                context.Response.ContentType = Application.Json;
+
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+
+                Log.Error(exceptionHandlerPathFeature?.Error, "Error happened");
+
+                if (exceptionHandlerPathFeature == null)
+                {
+                    return;
+                }
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(
+                    new
+                    {
+                        Message = app.Environment.IsDevelopment()
+                            ? JsonSerializer.Serialize(exceptionHandlerPathFeature.Error)
+                            : "Something went wrong"
+                    })
+                );
+            });
+        });
+
+    app.UseHttpsRedirection();
+
+    app.UseRouting();
+    app.UseHttpMetrics();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.MapMetrics();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-app.UseHttpMetrics();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapMetrics();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
